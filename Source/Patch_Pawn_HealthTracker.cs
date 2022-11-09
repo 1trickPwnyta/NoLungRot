b9f11c83be0a9acc00145df609690688703eaf70
Sarah Pace <sarahpace37@gmail.com>
2022-11-09 00:10:16 -0800

    Initial commit

---
+++ Source/Patch_Pawn_HealthTracker.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace NoLungRot
+{
+    [HarmonyPatch(typeof(Pawn_HealthTracker))]
+    [HarmonyPatch(nameof(Pawn_HealthTracker.AddHediff))]
+    [HarmonyPatch(new[] { typeof(Hediff), typeof(BodyPartRecord), typeof(DamageInfo), typeof(DamageWorker.DamageResult) })]
+    public static class Patch_Pawn_HealthTracker_AddHediff
+    {
+        public static bool Prefix(Hediff hediff)
+        {
+            return hediff.def != HediffDefOf.LungRotExposure;
+        }
+    }
+}
